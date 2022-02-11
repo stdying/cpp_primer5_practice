deffec3d456903b9615cf054691fd85a365778df
stdying <stdying@foxmail.com>
2022-02-11 14:39:42 +0800

    add:#13 类值拷贝

--- src/chapter13/chapter13.cpp
+++ src/chapter13/chapter13.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "chapter13.h"
+HasPtr &HasPtr::operator=(const HasPtr &rhs) {
+  //拷贝底层string
+  auto newP = new std::string(*rhs.ps);
+  //释放旧内存
+  delete ps;
+  ps = newP;
+  i = rhs.i;
+  //返回本对象
+  return *this;
+}
--- src/chapter13/chapter13.h
+++ src/chapter13/chapter13.h
@@ -4,6 +4,21 @@
 
 #ifndef CPP_PRACTICE_SRC_CHAPTER13_CHAPTER13_H_
 #define CPP_PRACTICE_SRC_CHAPTER13_CHAPTER13_H_
+#include <string>
+class HasPtr {
+ public:
+  HasPtr(const std::string &s = std::string()) :
+	  ps(new std::string(s)), i(0) {}
 
+  //对ps指向的string，每个HasPtr对象都有自己的拷贝
+  HasPtr(const HasPtr &p) :
+	  ps(new std::string(*p.ps)), i(p.i) {}
+  HasPtr &operator=(const HasPtr &);
+  ~HasPtr() { delete ps; }
+
+ private:
+  std::string *ps;
+  int i;
+};
 
 #endif //CPP_PRACTICE_SRC_CHAPTER13_CHAPTER13_H_
